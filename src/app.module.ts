5cd876b8bdc7baac0b5a03bc684b0ff8d0d10e2c
Koo In Young <zerokoo@lviscorp.com>
2023-10-11 20:49:22 +0900

    feat: initialize tasks controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, TasksModule],
   controllers: [AppController],
   providers: [AppService],
 })
